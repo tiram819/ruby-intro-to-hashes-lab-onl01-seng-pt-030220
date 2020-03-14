6b570f822f4c309bac4edc871c62ccfe95a898b5
Tabassum Hassan <tiram819@gmail.com>
2020-03-14 18:17:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,16 +25,8 @@ end
 
 def monopoly_with_third_tier
 	monopoly = {
-		:railroads=>
-		{
-		:pieces=>4,
-		:names=>
-		{
-		  :reading_railroad=>{},
-			:pennsylvania_railroad=>{},
-			:b_and_o_railroad=>{},
-			:shortline=>{}
-		},
+		
+	
 		
 		}
 
